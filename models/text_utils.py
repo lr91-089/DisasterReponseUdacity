e6a043c66f21c29b28dffd848513a59f772cef15
lr91-089 <luis.rochamobile@gmail.com>
2022-11-20 15:35:11 +0000

    Add missing files

---
+++ models/text_utils.py
@@ -0,0 +1,44 @@
+import re
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+import nltk
+nltk.download(['punkt', 'wordnet','stopwords','omw-1.4','averaged_perceptron_tagger'])
+
+
+def tokenize(text):
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    lower_text = text.lower().strip()
+    no_stop_words = re.sub(r"[^a-zA-Z0-9]", " ", lower_text) 
+    words = word_tokenize(no_stop_words)
+    words = [w for w in words if w not in stopwords.words("english")]
+    stemmed = [PorterStemmer().stem(w) for w in words]
+    lemmed = [WordNetLemmatizer().lemmatize(w, pos='v') for w in stemmed]
+    return lemmed
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            if len(pos_tags)>0:
+                first_word, first_tag = pos_tags[0]
+                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                    return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
